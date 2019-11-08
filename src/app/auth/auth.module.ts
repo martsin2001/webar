ecdaa301f296a13030e96492f71a1c5de4a7e7c4
Myroslav Martsin <forvaro@gmail.com>
2019-11-09 00:57:06 +0200

    create auth components

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'sign-in', pathMatch: 'full' },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'sign-up', component: SignUpComponent }
+];
 
 @NgModule({
   declarations: [SignInComponent, SignUpComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, RouterModule.forChild(routes)]
 })
-export class AuthModule { }
+export class AuthModule {}
