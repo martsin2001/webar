32aa6e98402c70f39465482a27441fd57a386e8f
Myroslav Martsin <forvaro@gmail.com>
2019-11-08 23:56:28 +0200

    setup project

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+
+@NgModule({
+  declarations: [SignInComponent, SignUpComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class AuthModule { }
