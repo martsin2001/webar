f72775daf89592c11d2982fa1db99dc8236f18d3
Myroslav Martsin <forvaro@gmail.com>
2019-11-10 14:33:32 +0200

    create sign up page

--- client/src/app/auth/components/sign-up/sign-up.component.ts
+++ client/src/app/auth/components/sign-up/sign-up.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  ValidationErrors
+} from '@angular/forms';
+import { AuthService } from '../../core/services/auth.service';
+import { SignUpCredentials } from '../../models/auth.interrface';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,10 +14,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent implements OnInit {
+  signUpForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private authService: AuthService) {}
 
   ngOnInit() {
+    this.initSignUpForm();
   }
 
+  createAccount() {
+    if (this.signUpForm.valid) {
+      this.authService.createAccount(this.signUpForm
+        .value as SignUpCredentials);
+    }
+  }
+
+  get emailError(): ValidationErrors {
+    return this.signUpForm.get('email').errors;
+  }
+
+  private initSignUpForm() {
+    this.signUpForm = this.fb.group({
+      firstName: [null, Validators.required],
+      lastName: [null, Validators.required],
+      email: [
+        null,
+        Validators.compose([Validators.required, Validators.email])
+      ],
+      password: [null, Validators.required],
+      confirmPassword: [null, Validators.required]
+    });
+  }
 }
